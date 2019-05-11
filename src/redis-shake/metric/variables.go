3428062f198bf97442b729465561b5b52b38642a
Vinllen Chen <cvinllen@gmail.com>
2019-05-11 16:40:55 +0800

    Feature 1.4 (#61)
    * bugfix: add metric in restore (#59)
    
    * Bugfix 1.4.3 (#60)
    
    * bugfix: add metric in restore
    
    * add ChangeLog

--- src/redis-shake/metric/variables.go
+++ src/redis-shake/metric/variables.go
@@ -34,6 +34,14 @@ type MetricRest struct {
 
 func NewMetricRest() []MetricRest {
 	detailMapList := runner.GetDetailedInfo().([]map[string]interface{})
+	if detailMapList == nil || len(detailMapList) == 0 {
+		return []{
+			MetricRest{
+				StartTime: utils.StartTime,
+				Status:    base.Status,
+			},
+		}
+	}
 
 	total := utils.GetTotalLink()
 	ret := make([]MetricRest, total)
