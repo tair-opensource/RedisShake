75e19e11bc6c65f3d27a7dcfad11d84dcb67171a
vinllen <cvinllen@gmail.com>
2019-10-28 18:39:47 +0800

    in debug CI 2

--- src/integration-test/inject/hash.go
+++ src/integration-test/inject/hash.go
@@ -29,12 +29,21 @@ func (th *TpHash) GetType() string {
 
 func (th *TpHash) Run() (map[string]interface{}, error) {
 	mp := make(map[string]interface{})
+	preDb := 0
 	for i := 1; i <= th.keyNum; i++ {
 		key := fmt.Sprintf("hash_%d", i)
 		sonMp := make(map[string]int)
 		for j := 1; j <= th.fieldNum; j++ {
 			field := fmt.Sprintf("field_%d", j)
 			val := th.r.Intn(th.keyNum)
+			db := th.r.Intn(16) // random db
+			if db != preDb {
+				if _, err := th.client.Do("select", db); err != nil {
+					return nil, fmt.Errorf("select db[%v] failed[%v]", db, err)
+				}
+				preDb = db
+			}
+
 			if _, err := th.client.Do("hset", key, field, val); err != nil {
 				return nil, fmt.Errorf("hset key[%v] value[%v] field[%v] failed[%v]", key, val, field, err)
 			}
@@ -42,5 +51,6 @@ func (th *TpHash) Run() (map[string]interface{}, error) {
 		}
 		mp[key] = sonMp
 	}
+
 	return mp, nil
 }
--- src/integration-test/inject/string.go
+++ src/integration-test/inject/string.go
@@ -27,9 +27,17 @@ func (ts *TpString) GetType() string {
 
 func (ts *TpString) Run() (map[string]interface{}, error) {
 	mp := make(map[string]interface{})
+	preDb := 0
 	for i := 1; i <= ts.keyNum; i++ {
 		key := fmt.Sprintf("string_%d", i)
 		val := ts.r.Intn(ts.keyNum)
+		db := ts.r.Intn(16) // random db
+		if db != preDb {
+			if _, err := ts.client.Do("select", db); err != nil {
+				return nil, fmt.Errorf("select db[%v] failed[%v]", db, err)
+			}
+			preDb = db
+		}
 
 		if _, err := ts.client.Do("set", key, val); err != nil {
 			return nil, fmt.Errorf("set key[%v] value[%v] failed[%v]", key, val, err)
