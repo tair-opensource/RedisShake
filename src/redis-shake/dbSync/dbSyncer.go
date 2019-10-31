29a2c7fc2ed525444da9d885dff9c15bbde7c1e5
vinllen <cvinllen@gmail.com>
2019-10-31 21:04:27 +0800

    split sync code from sync.go to dbSync directory to make code clean

---
+++ src/redis-shake/dbSync/dbSyncer.go
@@ -0,0 +1,125 @@
+package dbSync
+
+import (
+	"redis-shake/metric"
+	"os"
+	"redis-shake/common"
+	"redis-shake/base"
+	"io"
+	"pkg/libs/log"
+	"pkg/libs/io/pipe"
+	"redis-shake/heartbeat"
+	"bufio"
+
+	"redis-shake/configure"
+)
+
+// one sync link corresponding to one DbSyncer
+func NewDbSyncer(id int, source, sourcePassword string, target []string, targetPassword string, httpPort int) *DbSyncer {
+	ds := &DbSyncer{
+		id:              id,
+		source:          source,
+		sourcePassword:  sourcePassword,
+		target:          target,
+		targetPassword:  targetPassword,
+		httpProfilePort: httpPort,
+		WaitFull:        make(chan struct{}),
+	}
+
+	// add metric
+	metric.AddMetric(id)
+
+	return ds
+}
+
+type DbSyncer struct {
+	id int // current id in all syncer
+
+	source         string   // source address
+	sourcePassword string   // source password
+	target         []string // target address
+	targetPassword string   // target password
+
+	httpProfilePort int // http profile port
+
+	// stat info
+	stat Status
+
+	/*
+	 * this channel is used to calculate delay between redis-shake and target redis.
+	 * Once oplog sent, the corresponding delayNode push back into this queue. Next time
+	 * receive reply from target redis, the front node poped and then delay calculated.
+	 */
+	delayChannel chan *delayNode
+
+	sendBuf  chan cmdDetail // sending queue
+	WaitFull chan struct{}  // wait full sync done
+}
+
+func (ds *DbSyncer) GetExtraInfo() map[string]interface{} {
+	return map[string]interface{}{
+		"SourceAddress":      ds.source,
+		"TargetAddress":      ds.target,
+		"SenderBufCount":     len(ds.sendBuf),
+		"ProcessingCmdCount": len(ds.delayChannel),
+		"TargetDBOffset":     ds.stat.targetOffset.Get(),
+		"SourceDBOffset":     ds.stat.sourceOffset,
+	}
+}
+
+// main
+func (ds *DbSyncer) Sync() {
+	log.Infof("dbSyncer[%02d] starts syncing data from %v to %v with http[%v]",
+		ds.id, ds.source, ds.target, ds.httpProfilePort)
+
+	var sockfile *os.File
+	if len(conf.Options.SockFileName) != 0 {
+		sockfile = utils.OpenReadWriteFile(conf.Options.SockFileName)
+		defer sockfile.Close()
+	}
+
+	base.Status = "waitfull"
+	var input io.ReadCloser
+	var nsize int64
+	if conf.Options.Psync {
+		input, nsize = ds.sendPSyncCmd(ds.source, conf.Options.SourceAuthType, ds.sourcePassword, conf.Options.SourceTLSEnable)
+	} else {
+		input, nsize = ds.sendSyncCmd(ds.source, conf.Options.SourceAuthType, ds.sourcePassword, conf.Options.SourceTLSEnable)
+	}
+	defer input.Close()
+
+	log.Infof("dbSyncer[%02d] rdb file size = %d\n", ds.id, nsize)
+
+	if sockfile != nil {
+		r, w := pipe.NewFilePipe(int(conf.Options.SockFileSize), sockfile)
+		defer r.Close()
+		go func(r io.Reader) {
+			defer w.Close()
+			p := make([]byte, utils.ReaderBufferSize)
+			for {
+				utils.Iocopy(r, w, p, len(p))
+			}
+		}(input)
+		input = r
+	}
+
+	// start heartbeat
+	if len(conf.Options.HeartbeatUrl) > 0 {
+		heartbeatCtl := heartbeat.HeartbeatController{
+			ServerUrl: conf.Options.HeartbeatUrl,
+			Interval:  int32(conf.Options.HeartbeatInterval),
+		}
+		go heartbeatCtl.Start()
+	}
+
+	reader := bufio.NewReaderSize(input, utils.ReaderBufferSize)
+
+	// sync rdb
+	base.Status = "full"
+	ds.syncRDBFile(reader, ds.target, conf.Options.TargetAuthType, ds.targetPassword, nsize, conf.Options.TargetTLSEnable)
+
+	// sync increment
+	base.Status = "incr"
+	close(ds.WaitFull)
+	ds.syncCommand(reader, ds.target, conf.Options.TargetAuthType, ds.targetPassword, conf.Options.TargetTLSEnable)
+}
---
+++ src/redis-shake/dbSync/syncBegin.go
@@ -0,0 +1,153 @@
+package dbSync
+
+import (
+	"net"
+	"redis-shake/common"
+	"pkg/libs/log"
+	"time"
+	"pkg/libs/io/pipe"
+	"bufio"
+	"redis-shake/base"
+	"io"
+	"pkg/libs/atomic2"
+
+	"redis-shake/configure"
+)
+
+// send command to source redis
+
+func (ds *DbSyncer) sendSyncCmd(master, authType, passwd string, tlsEnable bool) (net.Conn, int64) {
+	c, wait := utils.OpenSyncConn(master, authType, passwd, tlsEnable)
+	for {
+		select {
+		case nsize := <-wait:
+			if nsize == 0 {
+				log.Infof("DbSyncer[%v] + waiting source rdb", ds.id)
+			} else {
+				return c, nsize
+			}
+		case <-time.After(time.Second):
+			log.Infof("DbSyncer[%v] - waiting source rdb", ds.id)
+		}
+	}
+}
+
+func (ds *DbSyncer) sendPSyncCmd(master, authType, passwd string, tlsEnable bool) (pipe.Reader, int64) {
+	c := utils.OpenNetConn(master, authType, passwd, tlsEnable)
+	log.Infof("DbSyncer[%v] psync connect '%v' with auth type[%v] OK!", ds.id, master, authType)
+
+	utils.SendPSyncListeningPort(c, conf.Options.HttpProfile)
+	log.Infof("DbSyncer[%v] psync send listening port[%v] OK!", ds.id, conf.Options.HttpProfile)
+
+	// reader buffer bind to client
+	br := bufio.NewReaderSize(c, utils.ReaderBufferSize)
+	// writer buffer bind to client
+	bw := bufio.NewWriterSize(c, utils.WriterBufferSize)
+
+	log.Infof("DbSyncer[%v] try to send 'psync' command", ds.id)
+	// send psync command and decode the result
+	runid, offset, wait := utils.SendPSyncFullsync(br, bw)
+	ds.stat.targetOffset.Set(offset)
+	log.Infof("DbSyncer[%v] psync runid = %s offset = %d, fullsync", ds.id, runid, offset)
+
+	// get rdb file size
+	var nsize int64
+	for nsize == 0 {
+		select {
+		case nsize = <-wait:
+			if nsize == 0 {
+				log.Infof("DbSyncer[%v] +", ds.id)
+			}
+		case <-time.After(time.Second):
+			log.Infof("DbSyncer[%v] -", ds.id)
+		}
+	}
+
+	// write -> pipew -> piper -> read
+	piper, pipew := pipe.NewSize(utils.ReaderBufferSize)
+
+	go func() {
+		defer pipew.Close()
+		p := make([]byte, 8192)
+		// read rdb in for loop
+		for rdbsize := int(nsize); rdbsize != 0; {
+			// br -> pipew
+			rdbsize -= utils.Iocopy(br, pipew, p, rdbsize)
+		}
+
+		for {
+			/*
+			 * read from br(source redis) and write into pipew.
+			 * Generally speaking, this function is forever run.
+			 */
+			n, err := ds.pSyncPipeCopy(c, br, bw, offset, pipew)
+			if err != nil {
+				log.PanicErrorf(err, "DbSyncer[%v] psync runid = %s, offset = %d, pipe is broken",
+					ds.id, runid, offset)
+			}
+			// the 'c' is closed every loop
+
+			offset += n
+			ds.stat.targetOffset.Set(offset)
+
+			// reopen 'c' every time
+			for {
+				// ds.SyncStat.SetStatus("reopen")
+				base.Status = "reopen"
+				time.Sleep(time.Second)
+				c = utils.OpenNetConnSoft(master, authType, passwd, tlsEnable)
+				if c != nil {
+					// log.PurePrintf("%s\n", NewLogItem("SourceConnReopenSuccess", "INFO", LogDetail{Info: strconv.FormatInt(offset, 10)}))
+					log.Infof("DbSyncer[%v] Event:SourceConnReopenSuccess\tId: %s\toffset = %d",
+						ds.id, conf.Options.Id, offset)
+					// ds.SyncStat.SetStatus("incr")
+					base.Status = "incr"
+					break
+				} else {
+					// log.PurePrintf("%s\n", NewLogItem("SourceConnReopenFail", "WARN", NewErrorLogDetail("", "")))
+					log.Errorf("DbSyncer[%v] Event:SourceConnReopenFail\tId: %s", ds.id, conf.Options.Id)
+				}
+			}
+			utils.AuthPassword(c, authType, passwd)
+			utils.SendPSyncListeningPort(c, conf.Options.HttpProfile)
+			br = bufio.NewReaderSize(c, utils.ReaderBufferSize)
+			bw = bufio.NewWriterSize(c, utils.WriterBufferSize)
+			utils.SendPSyncContinue(br, bw, runid, offset)
+		}
+	}()
+	return piper, nsize
+}
+
+func (ds *DbSyncer) pSyncPipeCopy(c net.Conn, br *bufio.Reader, bw *bufio.Writer, offset int64, copyto io.Writer) (int64, error) {
+	// TODO, two times call c.Close() ? maybe a bug
+	defer c.Close()
+	var nread atomic2.Int64
+	go func() {
+		defer c.Close()
+		for {
+			time.Sleep(time.Second * 1)
+			select {
+			case <-ds.WaitFull:
+				if err := utils.SendPSyncAck(bw, offset+nread.Get()); err != nil {
+					return
+				}
+			default:
+				if err := utils.SendPSyncAck(bw, 0); err != nil {
+					return
+				}
+			}
+		}
+	}()
+
+	var p = make([]byte, 8192)
+	for {
+		n, err := br.Read(p)
+		if err != nil {
+			return nread.Get(), nil
+		}
+		if _, err := copyto.Write(p[:n]); err != nil {
+			return nread.Get(), err
+		}
+		nread.Add(int64(n))
+	}
+}
