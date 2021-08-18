4974818c2bd65939a0b5936da335e9be30735001
suxb201 <sxb201@qq.com>
2021-08-18 11:00:24 +0800

    add regression testing & improve document (#356)
    * add test & improve doc
    
    * add test
    
    * remove old ci & modify build.sh
    
    * improve document
    
    * modify license
    
    * add action

--- build.sh
+++ build.sh
@@ -2,11 +2,23 @@
 
 set -o errexit
 
+# make sure we're in the directory where the project lives
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$PROJECT_DIR"
+
 MODULE_NAME=$(grep module src/go.mod |cut -d ' ' -f 2)
+
+# go version >=1.6
+go_version=$(go version | awk -F' ' '{print $3;}')
+bigVersion=$(echo $go_version | awk -F'[o.]' '{print $2}')
+midVersion=$(echo $go_version | awk -F'[o.]' '{print $3}')
+if [ $bigVersion -lt "1" -o $bigVersion -eq "1" -a $midVersion -lt "6" ]; then
+    echo "go version[$go_version] must >= 1.6"
+    exit 1
+fi
 
 # older version Git don't support --short !
 if [ -d ".git" ];then
-    #branch=`git symbolic-ref --short -q HEAD`
     branch=$(git symbolic-ref -q HEAD | awk -F'/' '{print $3;}')
     cid=$(git rev-parse HEAD)
 else
@@ -14,58 +26,27 @@ else
     cid="0.0"
 fi
 branch=$branch","$cid
+info="$MODULE_NAME/redis-shake/common.Version=$branch"
 
-export GOBIN=$(pwd)/bin/
-integration_test=$GOBIN/integration-test
-
-
-# make sure we're in the directory where the script lives
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
-
-info="redis-shake/common.Version=$branch"
 # golang version
-goversion=$(go version | awk -F' ' '{print $3;}')
-info=$info","$goversion
-bigVersion=$(echo $goversion | awk -F'[o.]' '{print $2}')
-midVersion=$(echo $goversion | awk -F'[o.]' '{print $3}')
-if  [ $bigVersion -lt "1" -o $bigVersion -eq "1" -a $midVersion -lt "6" ]; then
-    echo "go version[$goversion] must >= 1.6"
-    exit 1
-fi
+info=$info","$go_version
 
 t=$(date "+%Y-%m-%d_%H:%M:%S")
 info=$info","$t
 
 echo "[ BUILD RELEASE ]"
-run_builder='go build -v'
-
-main_package="$MODULE_NAME/redis-shake/main"
-
+BIN_DIR=$(pwd)/bin/
 cd src
 goos=(linux darwin windows)
-for g in "${goos[@]}"; do
+for g in "linux" "darwin" "windows";
+do
+    echo "try build GOOS=$g"
     export GOOS=$g
-    echo "try build goos=$g"
-
-    $run_builder -ldflags "-X $info" -o "$GOBIN/redis-shake.$g" $main_package
+    go build -v -ldflags "-X $info" -o "$BIN_DIR/redis-shake.$g" "$MODULE_NAME/redis-shake/main"
+    unset GOOS
     echo "build $g successfully!"
 done
-unset GOOS
 
-# build integration test
-$run_builder -o "${integration_test}/integration-test" "$MODULE_NAME/integration-test/main"
-cd ..
-# copy scripts
-cp scripts/start.sh ${GOBIN}/
-cp scripts/stop.sh ${GOBIN}/
-#cp scripts/run_direct.py ${output}/
-cp -r tools ${integration_test}/
-cp -r test ${integration_test}/
+cd $PROJECT_DIR
+cp conf/redis-shake.conf $BIN_DIR
 
-if [ "Linux" == "$(uname -s)" ];then
-	# hypervisor
-	gcc -Wall -O3 scripts/hypervisor.c -o ${output}/hypervisor -lpthread
-elif [ "Darwin" == "$(uname -s)" ];then
-	printf "\\nWARNING !!! MacOS doesn't supply hypervisor\\n"
-fi
---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+build hypervisor
+```shell
+gcc -Wall -O3 scripts/hypervisor.c -o hypervisor -lpthread
+```
