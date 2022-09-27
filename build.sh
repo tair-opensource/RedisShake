e9bf80f89564428e56cdbb68efd86a08487f6e27
suxb201 <sxb201@qq.com>
2022-09-27 18:52:45 +0800

    Update release.yml

--- build.sh
+++ build.sh
@@ -7,31 +7,35 @@ BIN_DIR=$(pwd)/bin/
 rm -rf "$BIN_DIR"
 mkdir -p "$BIN_DIR"
 
+cp sync.toml "$BIN_DIR"
+cp restore.toml "$BIN_DIR"
+cp -r filters "$BIN_DIR"
+cp -r scripts/cluster_helper "$BIN_DIR"
+
+dist() {
+    echo "try build GOOS=$1 GOARCH=$2"
+    export GOOS=$g
+    export GOARCH=$a
+    go build -v -trimpath -o "$BIN_DIR/redis-shake" "./cmd/redis-shake"
+    unset GOOS
+    unset GOARCH
+    echo "build success GOOS=$1 GOARCH=$2"
+
+    cd "$BIN_DIR"
+    tar -czvf ./redis-shake-"$1"-"$2".tar.gz ./sync.toml ./restore.toml ./redis-shake ./filters ./cluster_helper
+    cd ..
+}
+
+if [ "$1" == "dist" ]; then
+    echo "[ DIST ]"
+    for g in "linux" "darwin"; do
+        for a in "amd64" "arm64"; do
+            dist "$g" "$a"
+        done
+    done
+fi
+
 # build the current platform
 echo "try build for current platform"
 go build -v -trimpath -o "$BIN_DIR/redis-shake" "./cmd/redis-shake"
 echo "build success"
-
-
-if [ "$1" == "dist" ]; then
-  echo "[ DIST ]"
-  for g in "linux" "darwin"; do
-    for a in "amd64" "arm64"; do
-      echo "try build GOOS=$g GOARCH=$a"
-      export GOOS=$g
-      export GOARCH=$a
-      go build -v -trimpath -o "$BIN_DIR/redis-shake-$g-$a" "./cmd/redis-shake"
-      unset GOOS
-      unset GOARCH
-      echo "build success"
-    done
-  done
-  cp sync.toml "$BIN_DIR"
-  cp restore.toml "$BIN_DIR"
-  cp -r filters "$BIN_DIR"
-  cp -r scripts/cluster_helper "$BIN_DIR"
-  cd "$BIN_DIR"
-  tar -czvf ./redis-shake.tar.gz ./sync.toml ./restore.toml ./redis-shake-* ./filters ./cluster_helper
-  rm -rf ./filters
-  cd ..
-fi
