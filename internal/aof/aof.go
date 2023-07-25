2e10736064aee02d6b95e59a45ba99b7c70950b9
hwy1314 <1426317404@qq.com>
2023-07-25 10:01:20 +0800

    Implemented the basic framework of aof.go.

--- internal/aof/aof.go
+++ internal/aof/aof.go
@@ -3,17 +3,21 @@ package aof
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"log"
 	"strconv"
 	"unicode"
 
 	"github.com/alibaba/RedisShake/internal/entry"
+	"golang.org/x/tools/cmd/getgo/server"
 )
 
 const (
 	AofManifestFileTypeBase = "b" /* Base file */
 	AofManifestTypeHist     = "h" /* History file */
 	AofManifestTypeIncr     = "i" /* INCR file */
+	RDB_FORMAT_SUFFIX       = ".rdb"
+	AOF_FORMAT_SUFFIX       = ".aof"
 )
 
 /* AOF manifest definition */
@@ -34,7 +38,13 @@ func aofInfoCreate() *aofInfo {
 	return new(aofInfo)
 }
 
-func StringNeedsRepr(s string) int {
+var Aof_Info aofInfo = *aofInfoCreate()
+
+func (a *aofInfo) get_AofInfo_Name() string {
+	return a.fileName
+}
+
+func stringNeedsRepr(s string) int {
 	len := len(s)
 	point := 0
 	for len > 0 {
@@ -255,6 +265,11 @@ func aofInfoDup(orig *aofInfo) *aofInfo {
 	return ai
 }
 
+type listIter struct {
+	next      *listNode
+	direction int
+}
+
 type lists struct {
 	head, tail *listNode
 	len        uint64
@@ -272,6 +287,23 @@ func listCreate() *lists {
 	lists.tail = nil
 	lists.len = 0
 	return lists
+}
+func listNext(iter *listIter) *listNode {
+	current := iter.next
+
+	if current != nil {
+		if iter.direction == 0 {
+			iter.next = current.next
+		} else {
+			iter.next = current.prev
+		}
+	}
+	return current
+}
+
+func (list *lists) ListsRewind(li *listIter) {
+	li.next = list.head
+	li.direction = 0
 }
 
 type aofManifest struct {
@@ -329,10 +361,183 @@ func (ld *Loader) ParseRDB() int {
 	return 0
 }
 
+func stringcatprintf(s string, fmtStr string, args ...interface{}) string {
+	result := fmt.Sprintf(fmtStr, args...)
+	return s + result
+}
+
+func stringcatrepr(s string, p string, length int) string {
+	s = s + string("\"")
+	for i := 0; i < length; i++ {
+		switch p[i] {
+		case '\\', '"':
+			s = stringcatprintf(s, "\\%c", p[i])
+		case '\n':
+			s = s + "\\n"
+		case '\r':
+			s = s + "\\r"
+		case '\t':
+			s = s + "\\t"
+		case '\a':
+			s = s + "\\a"
+		case '\b':
+			s = s + "\\b"
+		default:
+			if strconv.IsPrint(rune(p[i])) {
+				s = s + string(p[i])
+			} else {
+				s = s + "\\x%02x"
+			}
+		}
+	}
+	return s + "\""
+}
+
+func aofInfoFormat(buf string, ai *aofInfo) string {
+	var filenameRepr string
+	if stringNeedsRepr(ai.fileName) == 1 {
+		filenameRepr = stringcatrepr("", ai.fileName, len(ai.fileName))
+	}
+	var ret string
+	if filenameRepr != "" {
+		ret = stringcatprintf(buf, "%s %s %s %d %s %c\n", AOF_MANIFEST_KEY_FILE_NAME, filenameRepr, AOF_MANIFEST_KEY_FILE_SEQ, ai.fileSeq, AOF_MANIFEST_KEY_FILE_TYPE, ai.aofFileType)
+	} else {
+		ret = stringcatprintf(buf, "%s %s %s %d %s %c\n", AOF_MANIFEST_KEY_FILE_NAME, ai.fileName, AOF_MANIFEST_KEY_FILE_SEQ, ai.fileSeq, AOF_MANIFEST_KEY_FILE_TYPE, ai.aofFileType)
+	}
+	return ret
+}
+
 func aofManifestcreate() *aofManifest {
 	am := &aofManifest{
 		incrAofList: listCreate(),
 		historyList: listCreate(),
 	}
 	return am
+}
+
+func listDup(orig *lists) *lists {
+	var copy *lists
+	var iter listIter
+	var node *listNode
+	copy = listCreate()
+	if copy == nil {
+		return nil
+	}
+	copy.ListsRewind(&iter)
+	node = listNext(&iter)
+	var value interface{}
+	for node != nil {
+		value = node.value
+	}
+
+	if listAddNodeTail(copy, value) == nil {
+		return nil
+	}
+	return copy
+}
+
+func AOFManifestDup(orig *aofManifest) *aofManifest {
+	if orig == nil {
+		panic("orig is nil")
+	}
+
+	am := &aofManifest{
+		currBaseFileSeq: orig.currBaseFileSeq,
+		currIncrFIleSeq: orig.currIncrFIleSeq,
+		dirty:           orig.dirty,
+	}
+
+	if orig.baseAofInfo != nil {
+		am.baseAofInfo = aofInfoDup(orig.baseAofInfo)
+	}
+
+	am.incrAofList = listDup(orig.incrAofList)
+	am.historyList = listDup(orig.historyList)
+
+	if am.incrAofList == nil || am.historyList == nil {
+		panic("IncrAOFlist or HistoryAOFlist is nil")
+	}
+	return am
+}
+
+func getAofManifestAsString(am *aofManifest) string {
+	if am == nil {
+		panic("am is nil")
+	}
+	var buf string
+	var ln *listNode
+	var li listIter
+
+	if am.baseAofInfo != nil {
+		buf = aofInfoFormat(buf, am.baseAofInfo)
+	}
+	am.historyList.ListsRewind(&li)
+	ln = listNext(&li)
+	for ln != nil {
+		ai, ok := ln.value.(*aofInfo)
+		if ok {
+			buf = aofInfoFormat(buf, ai)
+		}
+		ln = listNext(&li)
+	}
+
+	am.incrAofList.ListsRewind(&li)
+	ln = listNext(&li)
+	for ln != nil {
+		ai, ok := ln.value.(*aofInfo)
+		if ok {
+			buf = aofInfoFormat(buf, ai)
+		}
+		ln = listNext(&li)
+	}
+
+	return buf
+
+}
+
+func getNewBaseFileNameAndMarkPreAsHistory(am *aofManifest) string {
+	if am == nil {
+		log.Fatal("aofManifest is nil")
+	}
+	if am.baseAofInfo != nil {
+		if am.baseAofInfo.aofFileType != AofManifestFileTypeBase {
+			log.Fatal("base_aof_info has invalid file_type")
+		}
+		am.baseAofInfo.aofFileType = AofManifestTypeHist
+	}
+	var formatSuffix string
+	if server.aofUseRdbPreamble {
+		formatSuffix = RDB_FORMAT_SUFFIX
+	} else {
+		formatSuffix = AOF_FORMAT_SUFFIX
+	}
+	ai := aofInfoCreate()
+	ai.fileName = fmt.Sprintf("%s.%d%s%d", Aof_Info.get_AofInfo_Name(), am.currBaseFileSeq+1, BASE_FILE_SUFFIX, formatSuffix)
+	ai.fileSeq = am.currBaseFileSeq + 1
+	ai.aofFileType = AofManifestFileTypeBase
+	am.baseAofInfo = ai
+	am.dirty = 1
+	return am.baseAofInfo.fileName
+}
+
+// server 未处理
+func aofLoadManifestFromDisk() {
+	aof_manifest := aofManifestcreate()
+	if !difExists(server.aof_dirname) {
+		fmt.Printf("The AOF directory %s doesn't exist\n", server.AofDirname)
+		return
+	}
+
+	am_name := getAofManifestFileName()
+	am_filepath := makePath(server.aof_dirname, am_name)
+	if !fileExist(am_filepath) {
+		fmt.Printf("The AOF directory %s doesn't exist\n", server.AofDirname)
+		return
+	}
+
+	am := aofLoadManifestFromFile(am_filepath)
+	if am != nil {
+		aofManifestFreeAndUpdate(am)
+	}
+
 }
