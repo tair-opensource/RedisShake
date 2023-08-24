53f10582e1deef364d9b9743eeea8a17a667bcee
liyaojie <lyj199907@outlook.com>
2023-08-24 19:58:29 +0800

    add the support for the parse of tairString、tairHash and tairZset
    Signed-off-by: liyaojie <lyj199907@outlook.com>

--- internal/rdb/types/interface.go
+++ internal/rdb/types/interface.go
@@ -91,8 +91,7 @@ func ParseObject(rd io.Reader, typeByte byte, key string) RedisObject {
 		o.LoadFromBuffer(rd, key, typeByte)
 		return o
 	case rdbTypeModule, rdbTypeModule2: // module
-		o := new(ModuleObject)
-		o.LoadFromBuffer(rd, key, typeByte)
+		o := PareseModuleType(rd, key, typeByte)
 		return o
 	}
 	log.Panicf("unknown type byte: %d", typeByte)
