ad3c086cb32a09fad01959f92b31d6a51a1c9e4a
Manjusaka <me@manjusaka.me>
2024-01-02 09:47:49 +0800

    update comment

--- internal/function/function_test.go
+++ internal/function/function_test.go
@@ -7,6 +7,15 @@ import (
 	"RedisShake/internal/entry"
 )
 
+// BenchmarkRunFunction is a benchmark for RunFunction
+// Command is `go test -benchmem -bench="RunFunction$" -count=5 RedisShake/internal/function`
+// Output is:
+//
+// BenchmarkRunFunction-16             6741            182470 ns/op          234715 B/op       1079 allocs/op
+// BenchmarkRunFunction-16             7443            174567 ns/op          234710 B/op       1079 allocs/op
+// BenchmarkRunFunction-16             7101            178651 ns/op          234711 B/op       1079 allocs/op
+// BenchmarkRunFunction-16             6856            164739 ns/op          234722 B/op       1079 allocs/op
+// BenchmarkRunFunction-16             6804            174768 ns/op          234713 B/op       1079 allocs/op
 func BenchmarkRunFunction(b *testing.B) {
 	config.Opt = config.ShakeOptions{
 		Function: `
