e92b8de24e0767770630b9154eab6ba4b8dbfa97
饶乐佳 <raolejia1@ut.cn>
2024-11-25 11:50:53 +0800

    feat: support separate authentication for Sentinel and Redis
    - Add support for distinct username and password for Sentinel and Redis.
    - Resolve issues with connecting when Sentinel has no authentication but Redis does.
    - Handle scenarios where Sentinel and Redis have different authentication configurations.
    - Improve client logic to dynamically manage authentication based on configuration.
    
    BREAKING CHANGE: Requires configuration update to specify separate Sentinel and Redis credentials.

--- internal/writer/redis_standalone_writer.go
+++ internal/writer/redis_standalone_writer.go
@@ -18,14 +18,16 @@ import (
 )
 
 type RedisWriterOptions struct {
-	Cluster  bool   `mapstructure:"cluster" default:"false"`
-	Sentinel bool   `mapstructure:"sentinel" default:"false"`
-	Master   string `mapstructure:"master" default:""`
-	Address  string `mapstructure:"address" default:""`
-	Username string `mapstructure:"username" default:""`
-	Password string `mapstructure:"password" default:""`
-	Tls      bool   `mapstructure:"tls" default:"false"`
-	OffReply bool   `mapstructure:"off_reply" default:"false"`
+	Cluster          bool   `mapstructure:"cluster" default:"false"`
+	Sentinel         bool   `mapstructure:"sentinel" default:"false"`
+	Master           string `mapstructure:"master" default:""`
+	Address          string `mapstructure:"address" default:""`
+	Username         string `mapstructure:"username" default:""`
+	Password         string `mapstructure:"password" default:""`
+	SentinelUsername string `mapstructure:"sentinel_username" default:""`
+	SentinelPassword string `mapstructure:"sentinel_password" default:""`
+	Tls              bool   `mapstructure:"tls" default:"false"`
+	OffReply         bool   `mapstructure:"off_reply" default:"false"`
 }
 
 type redisStandaloneWriter struct {
